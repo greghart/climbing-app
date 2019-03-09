3e80f8f8242f417dc87dd6f086f09d6d7419dbfa
Greg Hart <greghartemail@gmail.com>
2019-03-09 00:44:55 +0000

    Setup markers to be draggable while still animating lines

--- src/typescript/redux/components/tracer/TrailTracer.tsx
+++ src/typescript/redux/components/tracer/TrailTracer.tsx
@@ -13,8 +13,17 @@ import BestTileLayer from '../BestTileLayer';
 import FixedContainerOverMap from '../layouts/FixedContainerOverMap';
 import SearchGroup from '../search/SearchGroup';
 import classNames = require('classnames');
+import { ExtractProps } from '../../../externals';
 
 const mapIcon = '<span><i class="fa fa-circle"/></span>';
+const selectedIcon = Leaflet.divIcon({
+  className: 'text-primary',
+  html: mapIcon,
+});
+const normalIcon = Leaflet.divIcon({
+  className: 'text-success',
+  html: mapIcon,
+});
 
 // We store our trail graph with lat lng literals as nodes and an adjacency list
 type Node = Leaflet.LatLngLiteral;
@@ -40,6 +49,70 @@ interface TrailTracerState {
   isDragging: boolean;
   // Current mouse position
   current: Leaflet.LatLng;
+}
+
+type DraggableMarkerProps = ExtractProps<typeof Marker> & {
+  onUpdate?: (node: Node) => unknown;
+  color?: string;
+  radius?: number;
+};
+/**
+ * A draggable marker that won't update while it's dragging.
+ *
+ * This allows us to re-render the parent container, but not interrupt Leaflet's drag
+ * workflow.
+ */
+class DraggableMarker extends React.Component<DraggableMarkerProps, { isDragging: boolean }> {
+
+  static defaultProps = {
+    onUpdate: (node) => false,
+    radius: 4
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isDragging: false
+    };
+    this.onDragStart = this.onDragStart.bind(this);
+    this.onDragEnd = this.onDragEnd.bind(this);
+    this.onDrag = this.onDrag.bind(this);
+  }
+
+  onDragStart(e: Leaflet.LeafletMouseEvent) {
+    // A drag click is not a click click
+    this.setState({ isDragging: true });
+  }
+
+  onDragEnd(e: Leaflet.DragEndEvent) {
+    this.props.onUpdate(e.target._latlng);
+    this.setState({ isDragging: false });
+  }
+
+  onDrag(e: Leaflet.DragEndEvent) {
+    // const point = this.state.nodes[nodeIndex];
+    // point.lat = e.target._latlng.lat;
+    // point.lng = e.target._latlng.lng;
+    // this.forceUpdate();
+    console.log(e, 'drag');
+    this.props.onUpdate(e.target._latlng);
+  }
+
+  shouldComponentUpdate() {
+    return !this.state.isDragging;
+  }
+
+  render() {
+    return (
+      <Marker
+        icon={selectedIcon}
+        {...this.props}
+        ondragstart={this.onDragStart}
+        ondragend={this.onDragEnd}
+        ondrag={this.onDrag}
+      />
+    );
+  }
 }
 
 /**
@@ -72,8 +145,6 @@ class TrailTracer extends React.Component<TrailTracerProps, TrailTracerState> {
     this.onClick = this.onClick.bind(this);
     this.onMouseMove = this.onMouseMove.bind(this);
     this.onKeyPress = this.onKeyPress.bind(this);
-    this.onDragSelectedStart = this.onDragSelectedStart.bind(this);
-    this.onDragSelectedEnd = this.onDragSelectedEnd.bind(this);
   }
 
   /**
@@ -196,26 +267,11 @@ class TrailTracer extends React.Component<TrailTracerProps, TrailTracerState> {
   }
 
   onKeyPress(e: React.KeyboardEvent<any>) {
-    // Turn manipulate on ctrl key, and off on release
     if (e.key === ' ') {
       this.setState({
         mode: this.state.mode === 'insert' ? 'manipulate' : 'insert'
       });
     }
-  }
-
-  onDragSelectedStart(nodeIndex: number) {
-    return (e: Leaflet.LeafletMouseEvent) => {
-    };
-  }
-
-  onDragSelectedEnd(nodeIndex: number) {
-    return (e: Leaflet.DragEndEvent) => {
-      const point = this.state.nodes[nodeIndex];
-      point.lat = e.target._latlng.lat;
-      point.lng = e.target._latlng.lng;
-      this.forceUpdate();
-    };
   }
 
   undo() {
@@ -262,20 +318,26 @@ class TrailTracer extends React.Component<TrailTracerProps, TrailTracerState> {
           ]}
         />
       ),
-      // Add node markers
+      // Add non selected node markers
       ...this.state.nodes.map((thisNode, index) => (
-        <Marker
-          key={`marker-${thisNode.lat}-${thisNode.lng}`}
-          icon={Leaflet.divIcon({
-            className: index === this.state.currentlySelected ? 'text-primary' : 'text-success',
-            html: mapIcon,
-          })}
+        <DraggableMarker
+          key={`marker-${index}`}
+          icon={index === this.state.currentlySelected ? selectedIcon : normalIcon}
           position={thisNode}
-          radius={4}
           color={index === this.state.currentlySelected ? 'green' : 'red'}
           draggable={this.state.mode === 'manipulate'}
-          ondragstart={this.onDragSelectedStart(index)}
-          ondragend={this.onDragSelectedEnd(index)}
+          onUpdate={(latlng) => {
+            const point = this.state.nodes[index];
+            point.lat = latlng.lat;
+            point.lng = latlng.lng;
+            this.forceUpdate();
+          }}
+          onclick={(e) => {
+            e.originalEvent.preventDefault();
+            this.setState({
+              currentlySelected: index
+            });
+          }}
         />
       ))
     ];
@@ -337,14 +399,15 @@ class TrailTracer extends React.Component<TrailTracerProps, TrailTracerState> {
                 autoComplete="off"
                 defaultChecked={this.state.mode === 'manipulate'}
                 onChange={(e) => this.setState({ mode: 'manipulate' })}
-              /> Manipulate
+              /> Select
             </label>
           </div>
         </div>
         <div className="col-12 col-sm-auto text-left">
           <p className="text-info small">
-            Hit Space to toggle "Manipulate" mode (or press on mobile). <br/>
-            This allows you to select your current node and move nodes around.
+            Hit Space to toggle "Select" mode (or press on mobile).
+            This allows you to select your base node to start a new trail branch,
+            or move existing nodes.
           </p>
         </div>
       </div>
