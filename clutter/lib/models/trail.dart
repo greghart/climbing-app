5f21c7224dab4244025811312200e05cf96ef712
Greg Hart <greghartemail@gmail.com>
2024-12-20 15:37:34 -0800

    Add a trail to the map
    Need to add layer controls for users who dont' want the noise

--- clutter/lib/models/trail.dart
+++ clutter/lib/models/trail.dart
@@ -1,3 +1,6 @@
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+
 import 'line.dart';
 import 'types.dart';
 
@@ -29,5 +32,22 @@ class Trail {
       'id': id,
       'lines': lines.map((line) => line.toJson()).toList(),
     };
+  }
+
+  List<Polyline<Object>> get toPolylines {
+    var polylines = <Polyline>[];
+    var pending = <LatLng>[
+      lines.first.start.toLatLng,
+      lines.first.end.toLatLng
+    ];
+    for (final line in lines.sublist(1)) {
+      // If not linear in setup (which is allowed), pivot to a new polyline
+      if (pending[pending.length - 1] != line.start.toLatLng) {
+        polylines.add(Polyline(points: pending));
+        pending = [line.start.toLatLng];
+      }
+      pending.add(line.end.toLatLng);
+    }
+    return polylines;
   }
 }
