df49a5d042a75618cdbe08d8f68c96ec0e44a68a
Greg Hart <greghartemail@gmail.com>
2024-12-30 11:48:00 -0800

    Refactor to have values internal to models lib

--- packages/ui/app/_components/search/DifficultySlider.tsx
+++ packages/ui/app/_components/search/DifficultySlider.tsx
@@ -2,6 +2,7 @@
 import { searchParamsParsers } from "@/app/_components/search/searchParams";
 import useQueryState from "@/app/_util/useQueryState";
 import { Slider } from "@mui/material";
+import { grades } from "models";
 
 export default function DifficultySlider() {
   const [difficultyMin, setDifficultyMin] = useQueryState(
@@ -12,22 +13,11 @@ export default function DifficultySlider() {
     "vMax",
     searchParamsParsers.vMax
   );
-  // Showing and storing v scale, will map to value under hood
   // VB - V17
-  const ticks = 18;
-
-  // TODO: Would be nice to use grades lib for this instead of knowing values
-  const marks = Array(ticks)
-    .fill(0)
-    .map((_, i) => {
-      const value = i * 10;
-      if (i % 2 === 1) return { value };
-      return {
-        value: value,
-        label: `${i === 0 ? "VB" : `V${i}`}`,
-      };
-    });
-  console.log(marks);
+  const marks = Object.values(grades.V).map((g) => ({
+    value: g.value,
+    label: g.raw,
+  }));
   return (
     <Slider
       aria-label="v scale"
